ebb4c3ddc2dc0982bc3eef82c2318e4791e3bcef
Mohnish Gorana <mohnishgorana1@gmail.com>
2025-03-16 12:24:48 +0530

    auth service init

---
+++ auth-service/src/app.ts
@@ -0,0 +1,36 @@
+import express from "express";
+import cors from "cors";
+import helmet from "helmet";
+import morgan from "morgan";
+// import rateLimit from "express-rate-limit";
+import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
+// import authRoutes from "./routes/auth.routes";
+import connectDB from "./config/db";
+
+dotenv.config();
+connectDB();
+
+const app = express();
+
+// Middleware
+app.use(cors());
+app.use(helmet());
+app.use(morgan("dev"));
+app.use(express.json());
+
+app.use(cookieParser());
+
+// Rate Limiting
+// const limiter = rateLimit({
+//   windowMs: 15 * 60 * 1000, // 15 minutes
+//   max: 100,
+//   message: "Too many requests, please try again later.",
+// });
+
+// app.use(limiter);
+
+// Routes
+// app.use("/api/v1/auth", authRoutes);
+
+export default app;
